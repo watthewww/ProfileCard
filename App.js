a8b99c1be23ee2220c60576c53e27565ae7990c0
Wat <chiptp1912@gmail.com>
2024-02-28 18:53:28 +0700

    init

--- App.js
+++ App.js
@@ -1,11 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Dimensions, StatusBar, Image, TouchableOpacity } from 'react-native';
+import StatBlock from './components/StatBlock';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+        <Image style={styles.avatar} source={require('./assets/profile.png')}/>
+      <View style={styles.userTitle}>
+        <Text style={styles.userName}>Vel Kumar P</Text>
+        <Text style={styles.userJob}>UI/UX Designer</Text>
+      </View>
+      <View style={styles.userStatContainer}>
+        <StatBlock title='Projects' count='11' ></StatBlock>
+        <StatBlock title='Following' count='250' ></StatBlock>
+        <StatBlock title='Followers' count='185' ></StatBlock>
+      </View>
+      <View style={styles.userInfoContainer}>
+        <Text style={styles.userInfo}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget ullamcorper arcu. Vestibulum iaculis interdum molestie. Suspendisse sodales, est vitae sollicitudin.</Text>
+      </View>
+      <View style={styles.socMedContainer}>
+        <Image style={styles.socMedIcon} source={require('./assets/facebook.png')}></Image>
+        <Image style={styles.socMedIcon} source={require('./assets/twitter.png')}></Image>
+        <Image style={styles.socMedIcon} source={require('./assets/google-plus.png')}></Image>
+      </View>
+      <View style={styles.viewContainer}>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>View Profile</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -15,6 +37,68 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
+    paddingTop: 50,
+  },
+  avatar: {
+    borderRadius: Math.round(Dimensions.get('window').width + Dimensions.get('window').height) / 2,
+    width: Dimensions.get('window').width * 0.3,
+    height: Dimensions.get('window').width * 0.3,
+    backgroundColor: 'grey',
+  },
+  userTitle: {
+    paddingTop: 30,
+    alignItems: 'center',
+  },
+  userName: {
+    fontSize: 30,
+  },
+  userJob: {
+    fontSize: 18,
+    color: 'grey'
+  },
+  userStatContainer: {
+    paddingTop: 30,
+    flexDirection: 'row',
+  },
+
+  userInfoContainer: {
+    paddingTop: 30,
+    paddingHorizontal: 25,
+  },
+  userInfo: {
+    textAlign: 'center',
+    color: 'grey'
+  },
+  socMedContainer: {
+    paddingTop: 30,
+    flexDirection: 'row',
     justifyContent: 'center',
   },
+  socMedIcon: {
+    margin: 10,
+    width: 30,
+    height: 30,
+  },
+  viewContainer: {
+    paddingTop: 30,
+    
+  },
+  button: {
+    borderRadius: 30,
+    alignItems: 'center',
+    paddingVertical: 10,
+    paddingHorizontal: 60,
+    backgroundColor: '#4dc4c1',
+    shadowOffset: {
+      width: 0,
+      height: 0,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 10,
+    elevation: 7,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+  }
 });
---
+++ components/StatBlock.js
@@ -0,0 +1,23 @@
+import { StyleSheet, Text, View } from "react-native";
+export default function StatBlock({ title, count }) {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.title}>{title}</Text>
+            <Text style={styles.count}>{count}</Text>
+        </View>
+    )
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+    },
+    title: {
+        color: 'grey',
+        fontSize: 18,
+
+    },
+    count: {
+        fontSize: 30,
+    }
+})
